d4030955b04984c337f3b2c41aae59131790c060
RodrigoVintem <rodrigo.vintem@tecnico.ulisboa.pt>
2024-07-20 10:19:34 +0100

    second_scene
    Improving second scene design

--- KennyGameJam_v3/Assets/Scripts/CameraFollow.cs
+++ KennyGameJam_v3/Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 
-    private Vector3 offset = new Vector3(0, 3, -10);
+    private Vector3 offset = new Vector3(0, 1, -10);
     public float smoothTime = 0.1f;
     private Vector3 velocity = Vector3.zero;
 
