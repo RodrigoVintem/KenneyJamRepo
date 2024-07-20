6653bf37b82e217934959a6a41d1529bef16f01d
RodrigoVintem <rodrigo.vintem@tecnico.ulisboa.pt>
2024-07-20 20:37:35 +0100

    working_second_scene

--- KennyGameJam_v3/Assets/Scripts/BoxSwitch.cs
+++ KennyGameJam_v3/Assets/Scripts/BoxSwitch.cs
@@ -28,15 +28,6 @@ public class BoxSwitch : MonoBehaviour
     void MoveToNextPoint()
     {
        platform.position = Vector2.MoveTowards(platform.position, points[goalPoint].position, moveSpeed * Time.deltaTime);
-
-       if(Vector2.Distance(platform.position, points[goalPoint].position) < 0.1f)
-       {
-           if(goalPoint == points.Count - 1){
-               goalPoint = 0;
-           }else{
-               goalPoint++;
-           }
-       }
     }
 
 
